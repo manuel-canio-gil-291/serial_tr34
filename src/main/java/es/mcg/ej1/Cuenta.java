ee8c5ba5bddb4e732e9364211b6d5bd02e6406e3
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-10-24 13:41:35 +0200

    Objeto Cuenta

---
+++ src/main/java/es/mcg/ej1/Cuenta.java
@@ -0,0 +1,57 @@
+package es.mcg.ej1;
+
+import java.io.Serializable;
+
+public class Cuenta implements Serializable
+{
+    /**
+     * Serial Version UID
+     */
+    private static final long serialVersionUID = 1l;
+    
+    private Estado estado;
+    private String nombre;
+    private int nCuenta;
+    
+    public Cuenta(String nombre, int nCuenta) 
+    {
+        this.nombre = nombre;
+        this.nCuenta = nCuenta;
+    }
+
+    public Estado getEstado() 
+    {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) 
+    {
+        this.estado = estado;
+    }
+
+    public String getNombre() 
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) 
+    {
+        this.nombre = nombre;
+    }
+
+    public int getnCuenta() 
+    {
+        return nCuenta;
+    }
+
+    public void setnCuenta(int nCuenta) 
+    {
+        this.nCuenta = nCuenta;
+    }
+
+    @Override
+    public String toString() {
+        return "Estado de la cuenta: " + estado + "\nNombre de la cuenta: " + nombre + "\nNº Cuenta: " + nCuenta + "";
+    }
+    
+}
