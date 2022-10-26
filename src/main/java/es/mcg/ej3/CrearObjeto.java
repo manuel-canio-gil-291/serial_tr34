c7fb46d56934a38727f12ce1371b3226e2a1e45a
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-10-26 17:25:35 +0200

    Creacion de objetos correos de antiguos alumnos

--- src/main/java/es/mcg/ej3/CrearObjeto.java
+++ src/main/java/es/mcg/ej3/CrearObjeto.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import es.mcg.ej2.AntiguoAlumno;
@@ -14,6 +16,7 @@ public class CrearObjeto {
         FileOutputStream fileOutputStream = null;
         ObjectOutputStream objectOutputStream = null;
         Scanner sc = new Scanner(System.in);
+        List<AntiguoAlumno> antiguoAlumnos = null;
 
         try 
         {
@@ -22,33 +25,52 @@ public class CrearObjeto {
             {
                 file.createNewFile();
             }
-            fileOutputStream = new FileOutputStream(file);
+            fileOutputStream = new FileOutputStream(file, false);
             objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            antiguoAlumnos = new ArrayList<AntiguoAlumno>();
+            boolean salir = false;
             String nombre, correo;
             double nota1,nota2,nota3;
-            System.out.print("Dime el nombre del alumno: ");
-            nombre = sc.nextLine();
-            System.out.print("Dime el correo del alumno: ");
-            correo = sc.nextLine();
-            System.out.println("Dime las notas del alumno");
-            System.out.print("Nota 1: ");
-            nota1 = sc.nextDouble();
-            System.out.print("Nota 2: ");
-            nota2 = sc.nextDouble();
-            System.out.print("Nota 3: ");
-            nota3 = sc.nextDouble();
-            AntiguoAlumno alumno = new AntiguoAlumno(nombre, nota1, nota2, nota3, correo);
-            if(alumno.validarEmail(correo))
-            {
-                System.out.println("El correo es valido");
-                objectOutputStream.writeObject(alumno);
-                System.out.println("Se ha guardado los datos en el fichero '"+file+"'");
-                System.out.println(alumno);
-            }
-            else
-            {
-                System.out.println("El correo no es valido");
-            }
+            do{
+                System.out.print("Dime el nombre del alumno: ");
+                nombre = sc.nextLine();
+                System.out.print("Dime el correo del alumno: ");
+                correo = sc.nextLine();
+                System.out.println("Dime las notas del alumno");
+                System.out.print("Nota 1: ");
+                nota1 = sc.nextDouble();
+                System.out.print("Nota 2: ");
+                nota2 = sc.nextDouble();
+                System.out.print("Nota 3: ");
+                nota3 = sc.nextDouble();
+                AntiguoAlumno alumno = new AntiguoAlumno(nombre, nota1, nota2, nota3, correo);
+                if(alumno.validarEmail(correo))
+                {
+                    System.out.println("El correo es valido");
+                    antiguoAlumnos.add(alumno);
+                }
+                else
+                {
+                    System.out.println("El correo no es valido");
+                }
+                char op;
+                System.out.print("Guardar mas datos? (s/n) ");
+                sc.nextLine();
+                op = sc.nextLine().charAt(0);
+                if(op == 'n')
+                {
+                    objectOutputStream.writeObject(antiguoAlumnos);
+                    salir = true;
+                }
+                else if(op == 's')
+                {
+                    salir = false;
+                }
+                else
+                {
+                    salir = false;
+                }
+            }while(!salir);
         } 
         catch (IOException ioException) 
         {
