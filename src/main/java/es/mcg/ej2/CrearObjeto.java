6070c2ab5ea3cedba7a759ebacf5fbc00c235dc6
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-10-26 20:46:36 +0200

    Creacion de objetos de notas de los alumnos

--- src/main/java/es/mcg/ej2/CrearObjeto.java
+++ src/main/java/es/mcg/ej2/CrearObjeto.java
@@ -7,12 +7,21 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+/**
+ * Esta clase crea las notas de los alumnos, los almacena en un List y se guarda en el fichero.
+ * @author Manuel Canio Gil
+ * @version 1.0
+ */
 public class CrearObjeto {
+    /**
+     * Metodo ejecutable que crea las tres notas del alumno.
+     * @param args
+     */
     public static void main(String[] args) {
         File file = null;
         FileOutputStream fileOutputStream = null;
         ObjectOutputStream objectOutputStream = null;
+        //Hacemos una lista de los alumnos
         List<Expediente> expedientes = null;
         Scanner sc = new Scanner(System.in);
         try 
@@ -29,8 +38,10 @@ public class CrearObjeto {
             objectOutputStream = new ObjectOutputStream(fileOutputStream);
             expedientes = new ArrayList<Expediente>();
             do{
+                //Pedimos los datos necesarios, primero el nombre del alumno
                 System.out.print("Dime el nombre del alumno: ");
                 nombre = sc.nextLine();
+                //Despues pedimos las tres notas para calcular la media
                 System.out.println("Dime las notas del alumno");
                 System.out.print("Nota 1: ");
                 nota1 = sc.nextDouble();
@@ -39,6 +50,7 @@ public class CrearObjeto {
                 System.out.print("Nota 3: ");
                 nota3 = sc.nextDouble();
                 Expediente expediente = new Expediente(nombre, nota1, nota2, nota3);
+                //Si la media de las tres da mayor que 7, los almacena en un List
                 if(expediente.calcularNotaMedia(nota1, nota2, nota3) > 7)
                 {
                     System.out.println("La nota media es superior de 7");
@@ -49,11 +61,13 @@ public class CrearObjeto {
                     System.out.println("La nota media es inferior de 7");
                 }
                 char op;
+                //Preguntamos si queremos guardar mas datos
                 System.out.print("Guardar mas datos? (s/n) ");
                 sc.nextLine();
                 op = sc.nextLine().charAt(0);
                 if(op == 'n')
                 {
+                    //Si decimos que no, guardamos la lista en el fichero
                     objectOutputStream.writeObject(expediente);
                     salir = true;
                 }
