8b7cc7067a62820bcbd54637769fc1af43469ee0
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-10-26 18:19:07 +0200

    Estado de la cuenta

--- src/main/java/es/mcg/ej1/Estado.java
+++ src/main/java/es/mcg/ej1/Estado.java
@@ -1,6 +1,11 @@
 package es.mcg.ej1;
-
+/**
+ * Esta clase define las enumeraciones del estado de la cuenta
+ * @author Manuel Canio Gil
+ * @version 1.0
+ */
 public enum Estado 
 {
     ALDIA, ATRASADA, DEUDOR
+    //Al dia, atrasada y deudor
 }
