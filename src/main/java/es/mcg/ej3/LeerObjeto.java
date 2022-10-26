3636090478952082536f0aecb92c1086123ad2a1
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-10-26 20:47:45 +0200

    Lectura de objetos del correo de los alumnos

--- src/main/java/es/mcg/ej3/LeerObjeto.java
+++ src/main/java/es/mcg/ej3/LeerObjeto.java
@@ -7,8 +7,17 @@ import java.io.ObjectInputStream;
 import java.util.List;
 
 import es.mcg.ej2.AntiguoAlumno;
-
+/**
+ * Esta clase lee la lista de las tres notas y el correo de los alumnos antiguos cargados en el fichero.
+ * 
+ * @author Manuel Canio Gil
+ * @version 1.0
+ */
 public class LeerObjeto {
+    /**
+     * Metodo ejecutable que lee la lista de datos de los antiguos alumnos cargados en el fichero.
+     * @param args
+     */
     public static void main(String[] args) {
         File file = null;
         FileInputStream fileInputStream = null;
@@ -19,9 +28,11 @@ public class LeerObjeto {
             file = new File("CorreosAlumnos.obj");
             fileInputStream = new FileInputStream(file);
             objectInputStream = new ObjectInputStream(fileInputStream);
+            //Leemos la lista de los datos de los antiguos alumnos
             antiguoAlumnos = (List<AntiguoAlumno>) objectInputStream.readObject();
             for(int i = 0; i < antiguoAlumnos.size(); i++)
             {
+                //Una vez leido mostramos los datos de los alumnos antiguos por la pantalla
                 System.out.println(antiguoAlumnos.get(i));
             }
         } 
