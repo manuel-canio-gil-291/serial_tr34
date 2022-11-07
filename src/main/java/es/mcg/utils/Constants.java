cdd34f859a95c6942c4c508b29f55c3e622f8a95
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-07 10:31:38 +0100

    Constantes

---
+++ src/main/java/es/mcg/utils/Constants.java
@@ -0,0 +1,6 @@
+package es.mcg.utils;
+
+public final class Constants {
+    public static final String EMAIL_REGEX = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+}
